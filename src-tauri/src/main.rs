5d2510098512bef4c8a2b8490da764afbe021195
mardenrafael <mardenrafaeldalmagro.gimenez@gmail.com>
2024-01-17 22:36:06 -0400

    :tada: intial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,21 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::SystemTray;
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+
+fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
+    let tray = SystemTray::new().with_menu(tray_menu);
+
+    tauri::Builder::default()
+        // .system_tray(tray)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="border border-red-500">
+      <p>Tetste</p>
+    </div>
+  );
+}
+
+export default App;
